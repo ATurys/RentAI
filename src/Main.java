6b81fb30d948679a647d35ee6abae6b68fedae26
ATurys <arithur300@gmail.com>
2025-05-05 19:47:54 -0300

    Planejamento da tarefa de acordo com os objetivos da atividade

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,30 @@ public class Main {
         System.out.println("Hello world!");
     }
 
+    /**
+     *Interface de usuário: terminal (console), deve conter opções numeradas que permitam ao usuário navegar pelas funcionalidades do sistema.
+     *
+     * Operações CRUD:
+     * Inserir (Create): Permitir o cadastro de novas instâncias das classes principais.
+     * Consultar (Read): Oferecer opções para listar todas as instâncias cadastradas de uma classe principal e/ou consultar os detalhes de uma instância específica.
+     *
+     * Persistência de Dados: Todos os dados cadastrados e consultados existirão apenas na memória RAM do computador durante a execução do programa. Ao encerrar a aplicação, os dados serão perdidos. O foco reside na lógica de negócio encapsulada nos objetos e na estrutura orientada a objetos do sistema.
+     *
+     * Tratamento de Erros: O sistema deve implementar validações básicas para a entrada de dados do usuário (por exemplo, garantir que um número seja digitado quando esperado, verificar formatos simples). Em caso de erro (entrada inválida, tentativa de consultar um objeto inexistente, etc.), o sistema deve fornecer mensagens claras e informativas ao usuário, evitando o encerramento abrupto do programa sempre que possível.
+     *
+     *
+     *Classes e Objetos: As operações do sistema serão realizadas através da criação (instanciação) e manipulação de objetos dessas classes.
+     *
+     * Encapsulamento:
+     * Obrigatoriedade: A aplicação correta do princípio do encapsulamento é obrigatória em todas as classes que contenham dados (atributos).
+     * Implementação: Os atributos (variáveis de instância) das classes devem ser declarados com modificadores de acesso restritivos (por exemplo, private). O acesso para leitura ou modificação desses atributos por código externo à classe deve ser controlado por getters e setters.
+     *
+     * Relacionamentos entre Classes:
+     * Obrigatoriedade: É obrigatório que o projeto contenha pelo menos um exemplo claro de associação e, adicionalmente, pelo menos um exemplo claro de agregação OU composição.
+     *
+     * Docs Auxiliar: https://docs.google.com/document/d/1uXVZb0l-LZmN_o0KLtty8QuYnvo3XpBttl1AFiik0Yg/edit?usp=sharing
+     */
+
+    String textoMenu = ""
+
 }
